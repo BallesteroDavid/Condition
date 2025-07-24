bd792b9b9a19fcb7d92963845c6b34cda6e8fe71
BallesteroDavid <david.ballestero@outlook.fr>
2025-07-24 13:43:43 +0200

    first

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// ici exo conditions
+
+
+// si exo1 est égal à 33, alors vous me faites un log qui affiche bordeaux sinon vous affichez Paris
+
+let exo1 = 33;
+
+'33' == 33
+
+// si exo1 est egal a 33 j'affiche bordeaux
+// sinon j'affiche madrid
+
+if(exo1 == 33){
+    console.log('bordeaux');
+}else{
+    console.log('bouuuu paris');
+
+}
+
+
